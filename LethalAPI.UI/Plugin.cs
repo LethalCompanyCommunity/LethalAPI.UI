26b31abbeb97333a1fa81f9c27b78f8dfa7284d0
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-11-30 14:33:30 -0600

    Add attributes and usings.

--- LethalAPI.UI/Plugin.cs
+++ LethalAPI.UI/Plugin.cs
@@ -54,6 +54,6 @@ public class Plugin : LethalAPI.Core.Features.Plugin<Config>
         }
 
         Instance = this;
-        Log.Info($"Started plugin &3LethalAPI.UI &gv1.0.0 &rby &7LethalAPI Modding Community&r.{(this.Config.Debug ? " [&2Debug&r]" : string.Empty)}");
+        Log.Debug($"Started {this.Name} Successfully.");
     }
 }
---
+++ LethalAPI.UI/Usings.cs
@@ -0,0 +1,10 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Usings.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+// ReSharper disable RedundantUsingDirective.Global
+#pragma warning disable SA1200 // File should appear in namespace declaration.
+global using Log = LethalAPI.Core.Log;
