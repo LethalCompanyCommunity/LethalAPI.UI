914ffd38bc8c0610049c3a55482157cd51d051d7
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-11-30 01:22:44 -0600

    Fix Styling

--- LethalAPI.UI/Plugin.cs
+++ LethalAPI.UI/Plugin.cs
@@ -1,14 +1,59 @@
-﻿using BepInEx;
+﻿// -----------------------------------------------------------------------
+// <copyright file="Plugin.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
 
-namespace LethalAPI.UI
+namespace LethalAPI.UI;
+
+using System;
+
+using HarmonyLib;
+using LethalAPI.Core;
+
+/// <summary>
+///     The main plugin class.
+/// </summary>
+public class Plugin : LethalAPI.Core.Features.Plugin<Config>
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    /// <summary>
+    ///     Gets the main <see cref="Plugin"/> instance.
+    /// </summary>
+    /// <remarks>
+    ///     This is optional. A custom implementation can replace this.
+    /// </remarks>
+    public static Plugin Instance { get; private set; } = null!;
+
+    /// <summary>
+    ///     Gets the main <see cref="Harmony"/> instance for the plugin.
+    /// </summary>
+    /// <remarks>
+    ///     This is optional. A custom implementation can replace this.
+    /// </remarks>
+    public static Harmony Harmony => new("com.LethalAPI.UI");
+
+    /// <inheritdoc />
+    public override string Name => "LethalAPI.UI";
+
+    /// <inheritdoc />
+    public override string Description => "PROJECT-DESCRIPTION";
+
+    /// <inheritdoc />
+    public override string Author => "LethalAPI Modding Community";
+
+    /// <inheritdoc />
+    public override Version Version => new(1, 0, 0);
+
+    /// <inheritdoc />
+    public override void OnEnabled()
     {
-        private void Awake()
+        if (!this.Config.IsEnabled)
         {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            return;
         }
+
+        Instance = this;
+        Log.Info($"Started plugin &3LethalAPI.UI &gv1.0.0 &rby &7LethalAPI Modding Community&r.{(this.Config.Debug ? " [&2Debug&r]" : string.Empty)}");
     }
 }
